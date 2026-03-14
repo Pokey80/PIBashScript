dc4d82d188de870d50573f712a1b37458f961467
Darrin Turner <darrin.turner97@gmail.com>
2026-03-14 17:14:55 -0500

    Added kubeadm and kubelet

--- SetupScript.sh
+++ SetupScript.sh
@@ -11,4 +11,4 @@ modprobe br_netfilter
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 chmod 644 /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubectl
+apt-get install -y kubectl kubeadm kubelet
